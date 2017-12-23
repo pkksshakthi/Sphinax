57bffadd9eaa2076dafae724e48e4fd3f80ac0cd
psak <pkksshakthi@gmail.com>
2017-12-24 00:07:44 +0530

    sakthivel project

---
+++ app/src/main/java/employee/hrms/sphinax/hrms/MainActivity.java
@@ -0,0 +1,41 @@
+package employee.hrms.sphinax.hrms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otp_generate);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
